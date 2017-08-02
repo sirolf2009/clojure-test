5961280032e15940833f11834120bc842f57d8f8
Floris <floris@northpool.nl>
2017-08-02 11:40:00 +0200

    stuff

--- src/clojure_test/core.clj
+++ src/clojure_test/core.clj
@@ -7,8 +7,8 @@
 (defmacro forever [& body]
   `(while true ~@body))
 
-(def database (influx/make-client {:db "orderbookBTCUSD" :host "localhost"}))
-(println "Connected to the database with a " (influx/ping) "ms ping")
+(def database (influx/make-client {:db "orderbookBTCUSD" :host "bigdatadev"}))
+(println "Connected to the database with a " (influx/ping database) "ms ping")
 (influx/create-db database)
 
 (def url "https://api.bitfinex.com/v1")
@@ -18,21 +18,23 @@
   (json/read-str (get (http/get (str url "/book/" symbol "?limit_bids=15&limit_asks=15&group=1")) :body))
   )
 
-(defn get-points [orders side]
+(defn get-points [orders side timestamp]
   (def as-point (fn [order]
                   {
                     :measurement "orderbook"
                     :tags {"exchange" "bitfinex" "index" (.indexOf orders order) "side" side}
                     :fields {"price" (get order "price") "amount" (get order "amount")}
-                    :timestamp (System/currentTimeMillis)
+                    :timestamp timestamp
                     }
                   ))
   (map as-point orders)
   )
 
 (defn get-orders [orderbook]
-  (def bids (get-points (get orderbook "bids") "bid"))
-  (def asks (get-points (get orderbook "asks") "ask"))
+  (def timestamp (System/currentTimeMillis))
+  (println timestamp)
+  (def bids (get-points (get orderbook "bids") "bid" timestamp))
+  (def asks (get-points (get orderbook "asks") "ask" timestamp))
   (concat bids asks)
   )
 
@@ -46,5 +48,6 @@
   )
 
 (defn -main [& args]
-  (forever (println "saving") (save BTCUSD) (Thread/sleep 1000))
+  (forever (save BTCUSD) (Thread/sleep 1000))
   )
+(-main)
--- src/clojure_test/orderbook.clj
+++ src/clojure_test/orderbook.clj
@@ -5,28 +5,40 @@
   (:use (incanter core stats charts io))
   (:gen-class))
 
-(def database (influx/make-client {:db "orderbookBTCUSD" :host "database"}))
+(def database (influx/make-client {:db "orderbookBTCUSD" :host "bigdatadev"}))
 
 (defn get-orderbook []
-  (let [response (influx/db-query database (str "SELECT * FROM orderbookBTCUSD.autogen.orderbook WHERE time > now() - 5m AND exchange='bitfinex'"))]
+  (let [response (influx/db-query database (str "SELECT * FROM orderbookBTCUSD.autogen.orderbook WHERE exchange='bitfinex'"))]
     (let [series (map (fn [series] (get series :series)) (get response :results))]
       (flatten series)
       )))
 
-(def series (first (get-orderbook)))
-(view (get series :values))
+;; (load "data")
 
-(def series-by-time (vec (partition-by (fn [row] (first row)) (get series :values))))
+(def series (map (fn [x] {
+                           :date (nth x 0)
+                           :amount (nth x 1)
+                           :exchange (nth x 2)
+                           :index (nth x 3)
+                           :price (nth x 4)
+                           :side (nth x 5)
+                           }) (get (first (get-orderbook)) :values)))
+series
+;; ({:date "2017-08-02T07:50:50.534Z", :amount "5.7", :exchange "bitfinex", :index "9", :price "2720.1", :side "bid"} {:date "2017-08-02T07:50:50.534Z", :amount "1.384", :exchange "bitfinex", :index "8", :price "2720.3", :side "bid"})
+
+(def series (list {:date "2017-08-02T07:50:50.534Z", :amount "5.7", :exchange "bitfinex", :index "9", :price "2720.1", :side "bid"} {:date "2017-08-02T07:50:50.534Z", :amount "1.384", :exchange "bitfinex", :index "8", :price "2720.3", :side "bid"} {:date "2017-08-02T07:50:51.534Z", :amount "5.7", :exchange "bitfinex", :index "9", :price "2720.1", :side "bid"} {:date "2017-08-02T07:50:51.534Z", :amount "1.384", :exchange "bitfinex", :index "8", :price "2720.3", :side "bid"}))
+
+(def series-by-time (partition-by #(get % :date) series))
 series-by-time
 
 (view (map (fn [orderbook]
-       (def by-side (group-by (fn [row] (nth row 5)) orderbook))
+       (def by-side (group-by :side orderbook))
        by-side
        (def bids (get by-side "bid"))
        (def asks (get by-side "ask"))
-       (def date (first (first bids)))
-       (def get-values (fn [row] {:index (nth row 3) :price (nth row 4) :amount (nth row 1)}))
-       (def bidswide (mapcat (fn [x] [(get x :price) (get x :amount)]) (sort-by :index (map get-values bids))))
-       (def askswide (mapcat (fn [x] [(get x :price) (get x :amount)]) (sort-by :index (map get-values asks))))
+       (def date (get (first bids) :date))
+       (println (first bids))
+       (def bidswide (mapcat (fn [x] [(get x :price) (get x :amount)]) (sort-by :index bids)))
+       (def askswide (mapcat (fn [x] [(get x :price) (get x :amount)]) (sort-by :index asks)))
        (concat [date] bidswide askswide)
        ) series-by-time))
